120e924537dd101ef4626b732f96f8ab08140b03
atenaiis <attennaisc@gmail.com>
2021-05-26 18:12:01 -0500

    work on js files

--- src/index.js
+++ src/index.js
@@ -1,57 +1,38 @@
-const addForm = document.querySelector('.add');
-const search = document.querySelector('.search input');
-const list = document.querySelector('.todos');
+import Controller from './controller';
 
-const generateTemplate = todo => {
-  const html = `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${todo}</span>
-      <i class="far fa-trash-alt delete"></i>
-    </li>
-  `;
-  list.innerHTML += html;
+
+window.onload = () => {
+  Controller.start();
 };
 
-const filterTodos = term => {
-
-  // add filtered class
-  Array.from(list.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.add('filtered'));
-
-  // remove filtered class
-  Array.from(list.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.remove('filtered'));
-
+window.addProject = () => {
+  Controller.addProject();
 };
 
-// add todos event
-addForm.addEventListener('submit', e => {
-  
-  e.preventDefault();
-  const todo = addForm.add.value.trim();
+window.addToDo = () => {
+  Controller.addToDo();
+};
 
-  if(todo.length){
-    generateTemplate(todo);
-    addForm.reset();
-  }
+window.deleteProject = (projectId) => {
+  Controller.deleteProject(projectId);
+};
 
-});
+window.deleteToDo = (projectId, toDoId) => {
+  Controller.deleteToDo(projectId, toDoId);
+};
 
-// delete todos event
-list.addEventListener('click', e => {
+window.onProjectTitleClick = (projectId) => {
+  Controller.toggleSaveBtn(projectId);
+};
 
-  if(e.target.classList.contains('delete')){
-    e.target.parentElement.remove();
-  }
+window.onProjectClickSave = (projectId) => {
+  Controller.updateProjectTitle(projectId);
+};
 
-});
+window.createSelectedModalList = (modalId, projectTitle) => {
+  Controller.addSelectedProjectsToModal(modalId, projectTitle);
+};
 
-// filter todos event
-search.addEventListener('keyup', () => {
-
-  const term = search.value.trim().toLowerCase();
-  filterTodos(term);
-
-});
+window.updateToDo = (modalId, projectId, toDoId) => {
+  Controller.updateToDo(modalId, projectId, toDoId);
+};
