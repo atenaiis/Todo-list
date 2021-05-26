83f017550e9ed85396cfbce3628fd804466cd433
atenaiis <attennaisc@gmail.com>
2021-05-26 14:38:00 -0500

    Add todo js file

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -18,13 +21,6 @@ module.exports = {
           'file-loader',
         ],
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
-      },
     ],
   },
-
 };
